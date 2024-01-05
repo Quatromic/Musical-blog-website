5e0894599db135425a21329bcea2d39abcbb03a0
Lawrence Muchiri <davidmuchiri@Lawrences-MacBook-Pro.local>
2024-01-05 17:14:40 +0300

    Began adding content to the website

---
+++ script.js
@@ -0,0 +1,60 @@
+const date = new Date()
+const datetag = document.getElementById("date")
+
+const day = (date) => {
+    switch(date.getDay()){
+        case 1:
+        default:
+            return "Mon";
+        case 2:
+            return "Tue"
+        case 3:
+            return "Wed"
+        case 4:
+            return "Thur"
+        case 5:
+            return "Fri"
+        case 6:
+            return "Sat"
+        case 7:
+            return "Sun"
+    }
+}
+
+const daydate = date.getDate()
+
+const month = (date) => {
+    switch(date.getMonth() + 1){
+        case 1:
+        default:
+            return "January"
+        case 2:
+            return "Feb"
+        case 3:
+            return "Mar"
+        case 4:
+            return "Apr"
+        case 5:
+            return "May"
+        case 6:
+            return "Jun"
+        case 7:
+            return "Jul"
+        case 8:
+            return "Aug"
+        case 9:
+            return "Sep"
+        case 10:
+            return "Oct"
+        case 11:
+            return "Nov"
+        case 12:
+            return "Dec"
+    }
+}
+
+const year = date.getFullYear()
+
+datetag.innerHTML += ` ${day(date)} ${daydate} ${month(date)} ${year}`
+
+console.log(`${day(date)} ${daydate} ${month(date)} ${year}`)
